7a828ccbb59aef19da8474b85775c0743b59f400
Ali Sina Shafiee <alisinashafieezada@gmail.com>
2023-03-26 23:17:13 +0330

    auth context removed

--- client/src/context/index.ts
+++ client/src/context/index.ts
@@ -1,2 +1,1 @@
 export { ColorProvider, colorContext } from './colors/ColorProvider';
-export { AuthProvider, authContext } from './auth/AuthProvider';
--- client/src/hooks/index.ts
+++ client/src/hooks/index.ts
@@ -1,2 +1,1 @@
-export { useAuth } from './useAuth';
 export { useColors } from './useColors';
