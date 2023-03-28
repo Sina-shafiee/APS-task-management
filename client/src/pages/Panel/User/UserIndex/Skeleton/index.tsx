6d2b79256bb8e355331602cad87c0bcbb7d7e0d0
Ali Sina Shafiee <alisinashafieezada@gmail.com>
2023-03-28 06:08:55 +0330

    skeleton refactor

--- client/src/pages/Panel/User/UserIndex/Skeleton/index.tsx
+++ client/src/pages/Panel/User/UserIndex/Skeleton/index.tsx
@@ -1,14 +1,23 @@
-import { Grid, Skeleton } from '@mui/material';
+import { Card, Grid, Skeleton } from '@mui/material';
 import { Container } from '@mui/system';
 
 const TasksSkeleton = () => {
   return (
     <Container maxWidth='lg' sx={{ marginTop: 6 }}>
-      <Skeleton width='170px' height='60px' animation='wave' />
-      <Grid spacing={2} container marginTop={-2} height='250px'>
+      <Skeleton width='170px' height='40px' animation='wave' />
+      <Grid rowSpacing={4} columnSpacing={2} container marginTop={4}>
         {new Array(4).fill(0).map((_, index) => (
           <Grid key={index} item xs={12} sm={6} md={4} lg={3}>
-            <Skeleton width='100%' height='100%' animation='wave' />
+            <Card sx={{ p: 2 }}>
+              <Skeleton width='150px' height='40px' animation='wave' />
+              <Skeleton width='100%' height='30px' animation='wave' />
+              <Skeleton
+                sx={{ mt: 2 }}
+                width='60px'
+                height='30px'
+                animation='wave'
+              />
+            </Card>
           </Grid>
         ))}
       </Grid>
