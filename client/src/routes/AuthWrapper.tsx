0e5a5ffbc43c03f5e93f56e8315357c6b89f3e59
Sina <alisinashafieezada@gmail.com>
2023-04-05 10:49:32 +0430

    update(:auth-checker-spinner)

--- client/src/routes/AuthWrapper.tsx
+++ client/src/routes/AuthWrapper.tsx
@@ -1,5 +1,5 @@
 import type { PropsWithChildren } from 'react';
-import { CircularProgress, Stack } from '@mui/material';
+import { LinearProgress } from '@mui/material';
 import { Navigate, useLocation } from 'react-router-dom';
 import { useQuery } from 'react-query';
 
@@ -15,7 +15,7 @@ const AuthWrapper = ({ children }: PropsWithChildren) => {
     onError: () => {
       console.clear();
     },
-    onSuccess: (data) => {
+    onSuccess: () => {
       console.clear();
     }
   });
@@ -23,17 +23,7 @@ const AuthWrapper = ({ children }: PropsWithChildren) => {
   const location = useLocation();
 
   if (isLoading) {
-    return (
-      <Stack
-        direction='row'
-        gap={1}
-        alignItems='center'
-        justifyContent='center'
-        sx={{ minHeight: '90vh' }}
-      >
-        <CircularProgress size={40} />
-      </Stack>
-    );
+    return <LinearProgress />;
   }
   if (
     isError &&
