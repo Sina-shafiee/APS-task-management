71d36c613ead36e0568db3ff3d4d45bdfa8d11b4
Sina <alisinashafieezada@gmail.com>
2023-04-08 12:05:16 +0430

    cloduinary config

--- server/server.js
+++ server/server.js
@@ -19,7 +19,7 @@ app.use(
   })
 );
 app.use(cookieParser());
-app.use(express.json());
+app.use(express.json({ limit: '10mb' }));
 
 app.use('/api/auth', authRouter);
 app.use('/api/tasks', taskRouter);
